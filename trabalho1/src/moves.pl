aceadc1fec04c94957408b017cac8a5723f327e2
theantonioalmeida <tony-alm@hotmail.com>
2017-11-04 23:31:51 +0000

    Implement attacked positions calculation for knight

--- trabalho1/src/moves.pl
+++ trabalho1/src/moves.pl
@@ -1,26 +1,54 @@
 :-include('board.pl').
 
 calculateAttackedPositions( Game ):-
-	getBoard( Game, Board ),
-	getAttackedBoard( Game, AttackedBoard ),
-	X is 0, Y is 0,
-	calculateAttackedPositionsAux( Board, AttackedBoard, X, Y, NewAttackedBoard ).
-	% needs to be completed %
+    getBoard( Game, Board ),
+    getAttackedBoard( Game, AttackedBoard ),
+    X is 0, Y is 0,
+    calculateAttackedPositionsAux( Board, AttackedBoard, X, Y, NewAttackedBoard ).
+    % needs to be completed %
 
 calculateAttackedPositionsAux( Board, AttackedPositions, X, Y, NewAttackedBoard ):-
-	diff(isEmpty( Board, X, Y ), true). % does this work?
-	% needs to be completed %
+    diff(isEmpty( Board, X, Y ), true). % does this work?
+    % needs to be completed %
 
-attackingPositions( AttackedPositions, KingInt, X, Y, FinalAttackedPositions ):-
-	isKing( KingInt ),
-	Y1 is Y-1,
-	Y2 is Y+1,
-	% not really making a movie, simply putting 1 on the given position %
-	makeMove( AttackedPositions, 1, X+1, Y, AttackedPositions2), % 1 means attacked position
-	makeMove( AttackedPositions2, 1, X-1, Y, AttackedPositions3), % 1 means attacked position
-	makeMove( AttackedPositions3, 1, X, Y1, AttackedPositions4), % 1 means attacked position
-	makeMove( AttackedPositions4, 1, X, Y2, FinalAttackedPositions), % 1 means attacked position
-	displayBoard(FinalAttackedPositions).
+% King
+attackingPositions( AttackedPositions, Piece, X, Y, FinalAttackedPositions ):-
+    isKing( Piece ),
+    write('King'),
+    Y1 is Y-1,
+    Y2 is Y+1,
+    % not really making a move, simply putting 1 on the given position %
+    makeMove( AttackedPositions, 'k', X+1, Y, AttackedPositions2), % 1 means attacked position
+    makeMove( AttackedPositions2, 'k', X-1, Y, AttackedPositions3), % 1 means attacked position
+    makeMove( AttackedPositions3, 'k', X, Y1, AttackedPositions4), % 1 means attacked position
+    makeMove( AttackedPositions4, 'k', X, Y2, FinalAttackedPositions), % 1 means attacked position
+    displayBoard(FinalAttackedPositions).
 
-isKing(1).
-isKing(6).
+% Knight
+attackingPositions( AttackedPositions, Piece, X, Y, FinalAttackedPositions ):-
+    isKnight( Piece ),
+    write('Knight'),
+    Yplus2 is Y+2,
+    Yminus2 is Y-2,
+    Yplus1 is Y+1,
+    Yminus1 is Y-1,
+
+    makeMove( AttackedPositions, 'k', X+1, Yplus2, AttackedPositions2 ),
+    makeMove( AttackedPositions2, 'k', X+1, Yminus2, AttackedPositions3 ),
+
+    makeMove( AttackedPositions3, 'k', X-1, Yplus2, AttackedPositions4 ),
+    makeMove( AttackedPositions4, 'k', X-1, Yminus2, AttackedPositions5 ),
+
+    makeMove( AttackedPositions5, 'k', X+2, Yplus1, AttackedPositions6 ),
+    makeMove( AttackedPositions6, 'k', X+2, Yminus1, AttackedPositions7 ),
+
+    makeMove( AttackedPositions7, 'k', X-2, Yplus1, AttackedPositions8 ),
+    makeMove( AttackedPositions8, 'k', X-2, Yminus1, FinalAttackedPositions ),
+    displayBoard( FinalAttackedPositions ).
+
+
+isKing('k').
+isKing('K').
+
+isKnight('N').
+isKnight('n').
