38ef057bb7074a1f97fb471a6a9b673fb03d3325
theantonioalmeida <tony-alm@hotmail.com>
2017-11-08 16:17:17 +0000

    Implement best immediate move calculation

--- trabalho1/src/ai.pl
+++ trabalho1/src/ai.pl
@@ -1,24 +1,35 @@
 getValidMove( Game, Player, Piece, X, Y, NewBoard ):-
 	getBoard( Game, Board ),
 	!,
-	validateMove(Game, Player, Piece, X, Y ), 
-	makeMove(Board, Piece, X, Y, NewBoard ).
+	validateMove(Game, Player, Piece, X, Y ).
 
-getOrderedMoves( Game, Player, MovesList ):-
- 	setof(Score-Piece-X-Y, 
- 		(getValidMove( Game, Player, Piece, X, Y, Board ), calcEvBoard( Board, Player, Score)),
- 		MovesList ).
-
- getMoves( Game, Player, MovesList ):-
+getMoves( Game, Player, MovesList ):-
  	findall(Piece-X-Y, 
- 		getValidMove(Game, Player, Piece, X, Y, _), MovesList ).
+ 		getValidMove(Game, Player, Piece, X, Y, _), UnsortedMoves ),
+ 	sort( UnsortedMoves, MovesList ).
 
- getMovesScore( Game, Player, MovesList ):-
- 	findall(Score-Piece-X-Y,
- 		(getValidMove( Game, Player, Piece, X, Y, Board ), calcEvBoard( Board, Player, Score)),
- 		MovesList ).
+evaluateAllMoves( Game, Player, MovesWithScore ):-
+	getMoves( Game, Player, MovesList ),
+	!,
+	getBoard( Game, Board ),
+	!,
+	evaluateAllMovesAux( MovesList, Board, Player, [], UnsortedMoves ),
+	!,
+	sort( UnsortedMoves, MovesWithScore ).
 
-calcEvBoard( Board, Player, Score ):-
-	updateAttackedBoard( Board, Player, AttackedBoard ),
+getBestMove( Game, Player, Piece, X, Y):-
+	evaluateAllMoves( Game, Player, MovesList ),
+	last( MovesList, _-Piece-X-Y ).
+
+evaluateAllMovesAux( [], _, _, ScoresList, ScoresList).
+
+evaluateAllMovesAux( [ Piece-X-Y | RestOfMoves ], Board, Player, TempScores, ScoresList ):-
+	evaluateMove( Player, Piece, X, Y, Board, Score ),
+	evaluateAllMovesAux( RestOfMoves, Board, Player, [ Score-Piece-X-Y | TempScores ], ScoresList).	
+
+evaluateMove(Player, Piece, X, Y, Board, Score):-
+	makeMove( Board, Piece, X, Y, NewBoard ),
+	updateAttackedBoard( NewBoard, Player, AttackedBoard ),
 	!,
 	evaluateBoard( AttackedBoard, Score ).
+
