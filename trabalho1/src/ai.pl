757f9549181847b4fcc3b3c421eb1e8956d471a6
theantonioalmeida <tony-alm@hotmail.com>
2017-11-10 23:26:34 +0000

    Base structure for more advanced AI

--- trabalho1/src/ai.pl
+++ trabalho1/src/ai.pl
@@ -37,6 +37,10 @@ getBestMove( Game, Player, Piece, X, Y):-
 	evaluateAllMoves( Game, Player, MovesList ),
 	last( MovesList, _-Piece-X-Y ).
 
+getBestMoveWithScore( Game, Player, Score, Piece, X, Y):-
+	evaluateAllMoves( Game, Player, MovesList ),
+	last( MovesList, Score-Piece-X-Y ).
+
 evaluateAllMoves( Game, Player, MovesWithScore ):-
 	getAllMoves( Game, Player, MovesList ), !,
 	getBoard( Game, Board ), !,
@@ -54,4 +58,71 @@ evaluateMove(Player, Piece, X, Y, Board, Score):-
 	updateAttackedBoard( NewBoard, Player, AttackedBoard ),
 	!,
 	evaluateBoard( AttackedBoard, Score ).
+
+
+magicShit( Game, Player, MagicMoves ):-
+	getAllMoves( Game, Player, MovesList ),
+	getOtherBestMoves( Game, Player, MovesList, [], OtherMoves ),
+	write('OTHER MOVES '),
+	write(OtherMoves),
+	otherPlayer( Player, Other ),
+	magicShit2( OtherMoves, Other, [], MagicMoves ).
+
+magicShit2( [], _, NewMoves, NewMoves).
+
+magicShit2( [ Piece-X-Y-Game | RestOfMoves ], Player, TempMoves, NewMoves ):-
+	
+	getBoard( Game, Board ),
+
+	displayBoard(Board),
+
+	% simulate game progression
+	makeMove( Board, Piece, X, Y, NextBoard ),	
+	write('make Move'), nl,
+	updateMadeMoves( Game, Player, Piece, GameTemp ),
+	write('update Move'), nl,
+	setBoard( GameTemp, NextBoard, GameTemp2 ),
+	write('set Board'), nl,
+	!,
+	displayBoard(NextBoard),
+	updateAttackedBoard( GameTemp2, GameTemp3 ),
+	write('update Attacked'), nl,
+	incTurnIndex( GameTemp3, GameTemp4 ),
+	write('incTurnIndex'), nl,
+	switchPlayer( GameTemp4, NewGame ),
+	write('switchPlayer'), nl,
+	otherPlayer( Player, Other ),
+	getBestMove( NewGame, Other, OtherPiece, OtherX, OtherY ),
+
+	magicShit2( RestOfMoves, Player, [ OtherPiece-OtherX-OtherY | TempMoves], NewMoves ).
+
+getOtherBestMoves( _, _, [], OthersMoves, OthersMoves).
+
+getOtherBestMoves( Game, Player, [ Piece-X-Y | RestOfMoves ], TempNewMoves, OthersMoves ):-
+	write('Current : '), write(Player-Piece-X-Y), nl,
+	getBoard( Game, Board ),
+	displayBoard(Board),
+
+	% simulate game progression
+	validateMove( Game, Player, Piece, X, Y ),
+	makeMove( Board, Piece, X, Y, NextBoard ),	
+	write('make Move'), nl,
+	updateMadeMoves( Game, Player, Piece, GameTemp ),
+	write('update Move'), nl,
+	setBoard( GameTemp, NextBoard, GameTemp2 ),
+	write('set Board'), nl,
+	!,
+	displayBoard(NextBoard),
+	updateAttackedBoard( GameTemp2, GameTemp3 ),
+	write('update Attacked'), nl,
+	incTurnIndex( GameTemp3, GameTemp4 ),
+	write('incTurnIndex'), nl,
+	switchPlayer( GameTemp4, NewGame ),
+	write('switchPlayer'), nl,
+
+	otherPlayer( Player, Other ),
+	write('Other player '), nl,
+	getBestMove( NewGame, Other, OtherPiece, OtherX, OtherY ),
+	write('Get best move '), nl,
+	getOtherBestMoves( Game, Player, RestOfMoves, [ OtherPiece-OtherX-OtherY-NewGame | TempNewMoves ], OthersMoves ).
 
