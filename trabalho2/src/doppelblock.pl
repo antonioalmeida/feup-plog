ea38fcde168bf01043ac778bce94b04ff0c30e66
cyrilico <jpdamas15@gmail.com>
2017-12-02 18:35:24 +0000

    Small restructure

--- trabalho2/src/doppelblock.pl
+++ trabalho2/src/doppelblock.pl
@@ -3,64 +3,56 @@
 
 solveInstance(N, ColumnsSum, LinesSum):-
     N > 2,
+
+    % Numbers can only go up to N-2
+    Limit is N-2,
+
+    % Get an empty NxN board
     getInitialBoard(N, Board),
 
-    ensureDomain(N, Board),
-    ensureBlackCells(Board),
-    ensureAllDistinct(Board),
+    % Ensure every value on the board is in range [0,N-2] (0 = black cell)
+    ensureDomain(Limit, Board),
+
+    % Ensure that in every row/column there are exactly two black cells
+    %ensureBlackCells(Board),
+
+    % Ensure that numbers in each row/column are distinct
+    ensureAllDistinct(Limit, Board),
 
     append(Board, Flattened),
     labeling([], Flattened),
     write(Board).
 
 % Ensures the domain of the variables used
-ensureDomain(N, Board):-
-    LimitN is N-2,
-    ensureDomainLine(LimitN, Board).
-
-ensureDomainLine(_, []).
-ensureDomainLine(Limit, [Line | Remaining]):-
+ensureDomain(_, []).
+ensureDomain(Limit, [Line | Remaining]):-
     domain(Line, 0, Limit),
-    ensureDomainLine(Limit, Remaining).
+    ensureDomain(Limit, Remaining).
 
 % Ensures that in a line/column there are exactly two black cells
-ensureBlackCells(Board):-
-    maplist(twoBlackCells, Board),
-    transpose(Board, TransposedBoard),
-    maplist(twoBlackCells, TransposedBoard).
-
-twoBlackCells(List):-
-    count(0, List, #=, 2).
+%ensureBlackCells(Board):-
+%    maplist(twoBlackCells, Board),
+%    transpose(Board, TransposedBoard),
+%    maplist(twoBlackCells, TransposedBoard).
+%twoBlackCells(List):-
+%    count(0, List, #=, 2).
 
 % Ensures that all elements in a line/column are different (black cells are ignored)
-ensureAllDistinct(Board):-
-    maplist(allDistinct, Board),
+ensureAllDistinct(Limit, Board):-
+    getValueAmountPairList(Limit, PairList),
+    write(PairList), nl,
+    maplist(allDistinct(PairList), Board),
     transpose(Board, TransposedBoard),
-    maplist(allDistinct, TransposedBoard).
+    maplist(allDistinct(PairList), TransposedBoard).
 
-allDistinct(List):-
-    deleteZeros(List, NList), %delete/3 wasn't working (apparently deprecated call)
-    all_distinct(NList).
+getValueAmountPairList(0, [0-2]).
+getValueAmountPairList(N, PairList):-
+    N1 is N-1,
+    getValueAmountPairList(N1, PairListTemp),
+    append(PairListTemp, [N-1], PairList).
 
-deleteZeros(List, NList):-
-    deleteZerosAux(List, [], NList).
-
-deleteZerosAux([], NList, NList).
-deleteZerosAux([Head | Tail], AccNList, NList):-
-    Head #\= 0, !,
-    append(AccNList, [Head], NewAccNList),
-    deleteZerosAux(Tail, NewAccNList, NList).
-deleteZerosAux([Head | Tail], AccNList, NList):-
-    deleteZerosAux(Tail, AccNList, NList).
-
-%allDistinctAux([], _).
-%allDistinctAux([Head | Tail], HeadValues):-
-%    Head #= 0, !,
-%    allDistinctAux(Tail, HeadValues).
-%allDistinctAux([Head | Tail], HeadValues):-
-%    \+ (member(Head, HeadValues)),
-%    append(HeadValues, [Head], NewHeadValues),
-%    allDistinctAux(Tail, NewHeadValues).
+allDistinct(PairList, Line):-
+    global_cardinality(Line, PairList).
 
 % Generate an empty NxN matrix represented by a list of lists
 getInitialBoard(N, Board):-
